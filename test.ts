ab95dc5144ede28f826da24a0b76b03874f35cfc
Bit_Blitz <rachit.tiwari.07.off@gmail.com>
2026-03-22 15:25:53 +0530

    HUHkkkggH

--- test.ts
+++
@@ -1,9 +0,0 @@
-//added
-
-export const processUserRequest = (user) => {
-  if (user.age > 18) {
-    console.log("User " + user.name.toUpperCase() + " is an adult.");
-    return true;
-  }
-  return false;
-}
