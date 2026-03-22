bc593703760764b424d7a9704fbf6cdba338fb94
Bit_Blitz <rachit.tiwari.07.off@gmail.com>
2026-03-22 14:38:35 +0530

    first commit

---
+++ src/limits.ts
@@ -0,0 +1,38 @@
+export class TokenLimits {
+  maxTokens: number
+  requestTokens: number
+  responseTokens: number
+  knowledgeCutOff: string
+
+  constructor(model = 'gpt-3.5-turbo') {
+    this.knowledgeCutOff = '2021-09-01'
+    if (model.includes('gpt-4o-mini')) {
+      this.maxTokens = 128000
+      this.responseTokens = 16000
+    } else if (model.includes('gpt-4o')) {
+      this.maxTokens = 128000
+      this.responseTokens = 4000
+    } else if (model.includes('llama3') || model.includes('groq')) {
+      this.maxTokens = 128000
+      this.responseTokens = 8000
+    } else if (model === 'gpt-4-32k') {
+      this.maxTokens = 32600
+      this.responseTokens = 4000
+    } else if (model === 'gpt-3.5-turbo-16k') {
+      this.maxTokens = 16300
+      this.responseTokens = 3000
+    } else if (model === 'gpt-4') {
+      this.maxTokens = 8000
+      this.responseTokens = 2000
+    } else {
+      this.maxTokens = 8000
+      this.responseTokens = 2000
+    }
+    // provide some margin for the request tokens
+    this.requestTokens = this.maxTokens - this.responseTokens - 100
+  }
+
+  string(): string {
+    return `max_tokens=${this.maxTokens}, request_tokens=${this.requestTokens}, response_tokens=${this.responseTokens}`
+  }
+}
